ba2dbee37f8fe3cb0bdc95fa9ae08951c10bcfed
hs-1971347-shinwoochul <113588811+hs-1971347-shinwoochul@users.noreply.github.com>
2024-04-21 23:51:35 +0900

    DB read

--- testpage/src/DBTestPageComponent/DBTestPage.js
+++ testpage/src/DBTestPageComponent/DBTestPage.js
@@ -1,30 +1,54 @@
-import React,{useState} from "react";
-import { ref, set } from "firebase/database";
-import { uid } from "uid";
+import React, { useState, useEffect } from "react";
+import { getDatabase, ref, onValue } from "firebase/database";
 import { db } from "../firebase";
 
-function DBTestPage(){
-    const [todo, setTodo] = useState("");
+function DBTestPage() {
+  const [userData, setUserData] = useState([]);
 
-  const handleTodoChange = e => {
-    setTodo(e.target.value);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // 데이터베이스에서 데이터 가져오기
+        const dbRef = ref(getDatabase());
+        onValue(dbRef, (snapshot) => {
+          const data = snapshot.val();
+          if (data) {
+            // 데이터가 존재할 경우 state 업데이트
+            setUserData(Object.values(data.user));
+          }
+        });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleMoneyChange = (index, newValue) => {
+    // 변경된 돈 값 업데이트
+    const newData = [...userData];
+    newData[index].money = newValue;
+    setUserData(newData);
+
+    // 데이터베이스에 변경된 값 업데이트
+    db.ref(`user/${index + 1}/money`).set(newValue);
   };
 
-  // Write
-  const writeData = () => {
-    const uuid = uid();
-
-    set(ref(db, "test/" + uuid), {
-      todo,
-      uuid,
-    });
-    setTodo("");
-  };
-  
   return (
-    <div className="App">
-      <input type="text" value={todo} onChange={handleTodoChange}></input>
-      <button onClick={writeData}>Submit</button>
+    <div>
+      {userData.map((user, index) => (
+        <div key={index}>
+          <p>User Name: {user.name}</p>
+          <p>Money: 
+            <input
+              type="number"
+              value={user.money}
+              onChange={(e) => handleMoneyChange(index, e.target.value)}
+            />
+          </p>
+        </div>
+      ))}
     </div>
   );
 }
--- testpage/src/firebase.js
+++ testpage/src/firebase.js
@@ -12,7 +12,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
 
 export default app;
 
-export const db = getDatabase(app);
+export {db};
